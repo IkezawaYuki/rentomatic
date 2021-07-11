32e1c7f6ad422bfd7b217b587f5bdd3ce7f35d15
yikezawa <y.ikezawa93@gmail.com>
2021-07-11 20:10:26 +0900

    mod

--- tests/domain/test_room.py
+++ tests/domain/test_room.py
@@ -1,5 +1,7 @@
+import json
 import uuid
 from rentomatic.domain.room import Room
+from rentomatic.serializers.room import RoomJsonEncoder
 
 
 def test_room_model_init():
@@ -17,3 +19,61 @@ def test_room_model_init():
     assert room.price == 10
     assert room.longitude == -0.09998975
     assert room.latitude == 51.75436293
+
+
+def test_room_model_from_dict():
+    code = uuid.uuid4()
+    init_dict = {
+        "code": code,
+        "size": 200,
+        "price": 10,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293,
+    }
+
+    room = Room.from_dict(init_dict)
+
+    assert room.code == code
+    assert room.size == 200
+    assert room.price == 10
+    assert room.longitude == -0.09998975
+    assert room.latitude == 51.75436293
+
+
+def test_room_model_to_dict():
+    init_dict = {
+        "code": uuid.uuid4(),
+        "size": 200,
+        "price": 10,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293,
+    }
+
+    room = Room.from_dict(init_dict)
+
+    assert room.to_dict() == init_dict
+
+
+def test_serialize_domain_room():
+    code = uuid.uuid4()
+
+    room = Room(
+        code,
+        size=200,
+        price=10,
+        longitude=-0.09998975,
+        latitude=51.75436293,
+    )
+
+    expected_json = f"""
+    {{
+        "code": "{code}",
+        "size": 200,
+        "price": 10,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293
+    }}
+    """
+
+    json_room = json.dumps(room, cls=RoomJsonEncoder)
+    assert json.loads(json_room) == json.loads(expected_json)
