f543d01057fadecf79fffa0b4bee20be5e5905d8
yikezawa <y.ikezawa93@gmail.com>
2021-07-13 22:49:07 +0900

    mod

--- application/app.py
+++ application/app.py
@@ -3,3 +3,10 @@ from flask import Flask
 from application.rest import room
 
 
+def create_app(config_name):
+    app = Flask(__name__)
+    config_module = f"application.config.{config_name.capitlize()}Config"
+    app.config.from_object(config_module)
+    app.register_blueprint(room.blue_print)
+    return app
+
