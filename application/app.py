d1f73e1b5aea78040f697240af2817a16d4df671
yikezawa <y.ikezawa93@gmail.com>
2021-07-15 22:48:14 +0900

    mod

--- application/app.py
+++ application/app.py
@@ -5,8 +5,8 @@ from application.rest import room
 
 def create_app(config_name):
     app = Flask(__name__)
-    config_module = f"application.config.{config_name.capitlize()}Config"
+    config_module = f"application.config.{config_name.capitalize()}Config"
     app.config.from_object(config_module)
-    app.register_blueprint(room.blue_print)
+    app.register_blueprint(room.blueprint)
     return app
 
