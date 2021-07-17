b7a1a88484307189bc1e81b6ac8d65a3e8f699ad
yikezawa <y.ikezawa93@gmail.com>
2021-07-17 14:13:11 +0900

    mod

--- tests/requests/test_room_list.py
+++ tests/requests/test_room_list.py
@@ -1,13 +1,18 @@
-from rentomatic.request.room_list import RoomListRequest
+from rentomatic.requests.room_list import build_room_list_request
 
 
 def test_build_room_list_request_without_parameters():
-    request = RoomListRequest()
+    request = build_room_list_request()
 
+    assert request.filter is None
     assert bool(request) is True
 
 
 def test_build_room_list_request_from_empty_dict():
-    request = RoomListRequest.from_dict({})
+    request = build_room_list_request({})
+    assert request.filters == 0
     assert bool(request) is True
 
+
+def test_build_room_list_request_with_invalid_filters_parameter():
+    request = build_room_list_request(filters=5)
